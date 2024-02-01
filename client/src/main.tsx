fde177fee5d26256e54acf1aa31be9ec395b1f28
thiyagarajanp98 <60639114+thiyagarajanp98@users.noreply.github.com>
2024-02-01 20:50:26 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import store from "../src/app/store.tsx";
 import { Provider } from "react-redux";
-
+import { fetchUserData } from "./features/homeData.tsx";
+store.dispatch(fetchUserData());
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
