51b7a5a768ffd6391e6cf02d03de17fe1efabd4c
thiyagarajanp98 <60639114+thiyagarajanp98@users.noreply.github.com>
2024-02-01 20:49:59 +0530

    Update homeData.tsx

--- client/src/features/homeData.tsx
+++ client/src/features/homeData.tsx
@@ -1,32 +1,47 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserData {
+  data: any;
+  loading: boolean;
+  error: string | undefined;
+}
+
 // Define the async thunk for fetching user data
 export const fetchUserData = createAsyncThunk(
   "user/fetchUserData",
   async () => {
-    const response = await fetch("https://x4hq7j-8080.csb.app");
+    const response = await fetch("https://kygdc3-8080.csb.app");
     const jsonData = await response.json();
     return jsonData;
   },
 );
+
 // Define the user slice
 export const userSlice = createSlice({
   name: "HomeData",
-  initialState: { data: null, loading: false, error: null },
+  initialState: { data: null, loading: false, error: undefined } as UserData,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchUserData.pending, (state) => {
         state.loading = true;
       })
-      .addCase(fetchUserData.fulfilled, (state, action) => {
+      .addCase(fetchUserData.fulfilled, (state, action: PayloadAction<any>) => {
         state.loading = false;
         state.data = action.payload;
       })
-      .addCase(fetchUserData.rejected, (state, action) => {
+      .addCase(fetchUserData.rejected, (state, action: PayloadAction<any>) => {
         state.loading = false;
         state.error = action.error.message;
       });
   },
 });
-export const selectAllHomeData = (state) => state.HomeData.data;
+
+export const selectAllHomeData = (state: { HomeData: UserData }) =>
+  state.HomeData.data;
+export const getHomeDataStatus = (state: { HomeData: UserData }) =>
+  state.HomeData.loading;
+export const getHomeDataError = (state: { HomeData: UserData }) =>
+  state.HomeData.error;
+
 export default userSlice.reducer;
